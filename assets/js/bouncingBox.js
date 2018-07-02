f28eb29cc20e4ec7c002b2840f9495759f2e167a
ethen001 <35516612+ethen001@users.noreply.github.com>
2018-07-01 21:29:51 -0400

    Enger-added-comments

--- assets/js/bouncingBox.js
+++ assets/js/bouncingBox.js
@@ -1,12 +1,12 @@
 var bouncingBox = (function() {
+	//body: the yellow square, environment: the greeen background
 	var box = {body: document.getElementById("box"), environment: document.getElementById("boxArea")};
-	var sliders = {label: document.querySelectorAll(".valueLabel"),
-					input: document.querySelectorAll("input"),
-					values: [0,0,0,0]};
+	//label: label of sliders, input: sliders input itself, values: stores previous value of slider to be used for taking the diff.
+	var sliders = {label: document.querySelectorAll(".valueLabel"),input: document.querySelectorAll("input"),values: [0,0,0,0]};
 	var xMotion = {speed: 1, pos: 0, posDirection: true, side: "left"};
 	var yMotion = {speed: 1, pos: 0, posDirection: true, side: "top"};
 
-	function realMove(motionType, dimensionName, size) {
+	function privateMove(motionType, dimensionName, size) {
 		var dimension  = parseInt(box.environment.style[dimensionName]) || size;
 		if(motionType.pos > dimension - 100) {
 			motionType.posDirection = false;
@@ -20,13 +20,15 @@ var bouncingBox = (function() {
 		}
 		box.body.style[motionType.side] = motionType.pos + "px";
 	}
-
+	
+	//method added to sliders object to initialize values array to corrrect state
 	sliders.initializeValues = function() {
 		for (var i = 0; i < this.values.length; i++) {
 			this.values[i] = parseInt(this.input[i].value);
 		}
 	}
-
+	
+	//add proper event listener to each slider input
 	function RangeInputEventListeners() {
 		for (var i = 0; i < sliders.input.length; i++) {
 			sliders.input[i].addEventListener("change",chooseEventListener(sliders.input[i]));
@@ -52,7 +54,7 @@ var bouncingBox = (function() {
 		}
 		return func;
 	}
-
+	//this increases the width and height of green box
 	function eventListener(rangeInput) {
 		var sizes = {width: 600, height: 400};
 		var digit = {width: 0, height: 1};
@@ -67,7 +69,7 @@ var bouncingBox = (function() {
 			sliders.label[digit].textContent = newValue;			
 		}
 	}
-
+	//calculate the new speed of the box
 	function getSpeed(rangeInput, motion, i) {
 		var newValue = parseInt(rangeInput.value);
 		motion.speed = newValue / 10;
@@ -80,13 +82,13 @@ var bouncingBox = (function() {
 			RangeInputEventListeners();
 		},
 		move: function() {
-			realMove(xMotion, "width", 600);
-			realMove(yMotion, "height", 400);
+			privateMove(xMotion, "width", 600);
+			privateMove(yMotion, "height", 400);
 		}
 	}	
 })();
 
 bouncingBox.initialize();
-setInterval(bouncingBox.move, 10);
+setInterval(bouncingBox.move, 10); //  this will run the animation in intevals of 10 milliseconds
 
 
